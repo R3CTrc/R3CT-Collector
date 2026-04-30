097cc6ad94bf9c4383c1520aa2bdc1dd38a37632
R3CT <radekrc4@gmail.com>
2026-04-30 21:16:57 +0200

    Added Trophy blocks

--- common/src/main/java/com/r3ct/collector/logic/ServerItemHandler.java
+++ common/src/main/java/com/r3ct/collector/logic/ServerItemHandler.java
@@ -5,6 +5,7 @@ import com.r3ct.collector.data.ModState;
 import com.r3ct.collector.data.PlayerData;
 import com.r3ct.collector.network.LeaderboardDataPayload;
 import com.r3ct.collector.platform.Services;
+import net.minecraft.ChatFormatting;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.Identifier;
 import net.minecraft.server.level.ServerPlayer;
@@ -45,12 +46,16 @@ public class ServerItemHandler {
         Inventory inv = player.getInventory();
         boolean foundAndRemoved = false;
 
-        for (int i = 0; i < inv.getContainerSize(); i++) {
-            ItemStack stack = inv.getItem(i);
-            if (stack.is(targetItem)) {
-                stack.shrink(1);
-                foundAndRemoved = true;
-                break;
+        if (player.isCreative()) {
+            foundAndRemoved = true;
+        } else {
+            for (int i = 0; i < inv.getContainerSize(); i++) {
+                ItemStack stack = inv.getItem(i);
+                if (stack.is(targetItem)) {
+                    stack.shrink(1);
+                    foundAndRemoved = true;
+                    break;
+                }
             }
         }
 
@@ -117,7 +122,26 @@ public class ServerItemHandler {
         if (rewardItemId != null) {
             Item rewardItem = BuiltInRegistries.ITEM.get(Identifier.parse(rewardItemId)).map(net.minecraft.core.Holder::value).orElse(Items.AIR);
             if (rewardItem != Items.AIR) {
-                giveItemToPlayer(player, new ItemStack(rewardItem, 1));
+                ItemStack rewardStack = new ItemStack(rewardItem, 1);
+
+                net.minecraft.network.chat.MutableComponent customName = net.minecraft.network.chat.Component.literal(player.getName().getString())
+                        .withStyle(net.minecraft.ChatFormatting.AQUA);
+
+                customName.append(net.minecraft.network.chat.Component.literal(" - ").withStyle(net.minecraft.ChatFormatting.LIGHT_PURPLE));
+
+                customName.append(net.minecraft.network.chat.Component.translatable(rewardItem.getDescriptionId()).withStyle(net.minecraft.ChatFormatting.LIGHT_PURPLE));
+
+                if (rewardItemId.equals("r3ct_collector:trophy_mod")) {
+                    net.minecraft.world.item.CreativeModeTab tab = BuiltInRegistries.CREATIVE_MODE_TAB.get(Identifier.parse(tabId)).map(net.minecraft.core.Holder::value).orElse(null);
+                    if (tab != null) {
+                        customName.append(net.minecraft.network.chat.Component.literal(" - ").withStyle(net.minecraft.ChatFormatting.LIGHT_PURPLE))
+                                .append(tab.getDisplayName().copy().withStyle(net.minecraft.ChatFormatting.LIGHT_PURPLE));
+                    }
+                }
+
+                rewardStack.set(net.minecraft.core.component.DataComponents.CUSTOM_NAME, customName);
+
+                giveItemToPlayer(player, rewardStack);
 
                 data.rewardedCategories.add(tabId);
 
--- neoforge/src/main/java/com/r3ct/collector/CollectorNeoForge.java
+++ neoforge/src/main/java/com/r3ct/collector/CollectorNeoForge.java
@@ -6,13 +6,23 @@ import com.r3ct.collector.config.CollectorRewardsConfig;
 import com.r3ct.collector.logic.ServerItemHandler;
 import com.r3ct.collector.network.SubmitItemPayload;
 import com.r3ct.collector.network.SyncDataPayload;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.item.Rarity;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.entity.player.PlayerEvent;
 import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
 import net.neoforged.neoforge.network.registration.PayloadRegistrar;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.Identifier;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.neoforged.neoforge.registries.RegisterEvent;
+import com.r3ct.collector.block.ModBlocks;
 
 import java.util.HashSet;
 
@@ -24,6 +34,7 @@ public class CollectorNeoForge {
         CollectorRewardsConfig.load();
 
         modEventBus.addListener(this::registerPackets);
+        modEventBus.addListener(this::onRegister);
         NeoForge.EVENT_BUS.addListener(this::onPlayerJoin);
     }
 
@@ -70,5 +81,56 @@ public class CollectorNeoForge {
             com.r3ct.collector.data.PlayerData data = com.r3ct.collector.data.ModState.getPlayerData(serverPlayer.level().getServer(), serverPlayer.getUUID());
             com.r3ct.collector.platform.Services.PLATFORM.sendSyncDataPacketToClient(serverPlayer, data.unlockedItems, data.rewardedCategories);
         }
+    }
+
+    private void onRegister(RegisterEvent event) {
+
+        java.util.function.BiConsumer<String, Block> registerTrophy = (name, block) -> {
+            Identifier id = Identifier.parse(Constants.MOD_ID + ":" + name);
+            ResourceKey<Item> itemKey = ResourceKey.create(Registries.ITEM, id);
+
+            event.register(BuiltInRegistries.BLOCK.key(), helper -> helper.register(id, block));
+            event.register(BuiltInRegistries.ITEM.key(), helper -> helper.register(id, new BlockItem(block, new Item.Properties()
+                    .setId(itemKey).stacksTo(1).rarity(Rarity.EPIC).fireResistant()
+            )));
+        };
+
+        registerTrophy.accept("trophy_building", ModBlocks.TROPHY_BUILDING);
+        registerTrophy.accept("trophy_combat", ModBlocks.TROPHY_COMBAT);
+        registerTrophy.accept("trophy_tools", ModBlocks.TROPHY_TOOLS);
+        registerTrophy.accept("trophy_food", ModBlocks.TROPHY_FOOD);
+        registerTrophy.accept("trophy_redstone", ModBlocks.TROPHY_REDSTONE);
+        registerTrophy.accept("trophy_ingredients", ModBlocks.TROPHY_INGREDIENTS);
+        registerTrophy.accept("trophy_natural", ModBlocks.TROPHY_NATURAL);
+        registerTrophy.accept("trophy_colored", ModBlocks.TROPHY_COLORED);
+        registerTrophy.accept("trophy_egg", ModBlocks.TROPHY_EGG);
+        registerTrophy.accept("trophy_functional", ModBlocks.TROPHY_FUNCTIONAL);
+        registerTrophy.accept("trophy_mod", ModBlocks.TROPHY_MOD);
+
+        event.register(BuiltInRegistries.BLOCK_ENTITY_TYPE.key(), helper -> {
+            helper.register(Identifier.parse(Constants.MOD_ID + ":trophy_building_be"), ModBlocks.TROPHY_BE_TYPE);
+        });
+
+        event.register(Registries.CREATIVE_MODE_TAB, helper -> {
+            helper.register(Identifier.parse(Constants.MOD_ID + ":main_tab"),
+                    net.minecraft.world.item.CreativeModeTab.builder()
+                            .title(net.minecraft.network.chat.Component.translatable("itemGroup." + Constants.MOD_ID + ".main_tab"))
+                            .icon(() -> new net.minecraft.world.item.ItemStack(ModBlocks.TROPHY_BUILDING))
+                            .displayItems((context, output) -> {
+                                output.accept(ModBlocks.TROPHY_BUILDING);
+                                output.accept(ModBlocks.TROPHY_NATURAL);
+                                output.accept(ModBlocks.TROPHY_COLORED);
+                                output.accept(ModBlocks.TROPHY_COMBAT);
+                                output.accept(ModBlocks.TROPHY_TOOLS);
+                                output.accept(ModBlocks.TROPHY_REDSTONE);
+                                output.accept(ModBlocks.TROPHY_FUNCTIONAL);
+                                output.accept(ModBlocks.TROPHY_FOOD);
+                                output.accept(ModBlocks.TROPHY_INGREDIENTS);
+                                output.accept(ModBlocks.TROPHY_EGG);
+                                output.accept(ModBlocks.TROPHY_MOD);
+                            })
+                            .build()
+            );
+        });
     }
 }
