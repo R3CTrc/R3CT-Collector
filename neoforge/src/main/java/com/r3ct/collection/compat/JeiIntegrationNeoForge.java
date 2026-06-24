23166161f7cf498de73327a3c068e20336a221ac
R3CT <radekrc4@gmail.com>
2026-06-24 14:15:42 +0200

    Added jei integration

---
+++ neoforge/src/main/java/com/r3ct/collection/compat/JeiIntegrationNeoForge.java
@@ -0,0 +1,58 @@
+package com.r3ct.collection.compat;
+
+import com.r3ct.collection.Constants;
+import com.r3ct.collection.client.screen.CatalogScreen;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.gui.builder.IClickableIngredientFactory;
+import mezz.jei.api.registration.IGuiHandlerRegistration;
+import mezz.jei.api.runtime.IClickableIngredient;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.renderer.Rect2i;
+import net.minecraft.resources.Identifier;
+import net.minecraft.world.item.ItemStack;
+
+import java.util.Optional;
+
+@JeiPlugin
+public class JeiIntegrationNeoForge implements IModPlugin {
+    @Override
+    public Identifier getPluginUid() {
+        return Identifier.parse(Constants.MOD_ID + ":jei_plugin");
+    }
+
+    @Override
+    public void registerGuiHandlers(IGuiHandlerRegistration registration) {
+
+        registration.addGuiScreenHandler(CatalogScreen.class, new mezz.jei.api.gui.handlers.IScreenHandler<CatalogScreen>() {
+            @Override
+            public mezz.jei.api.gui.handlers.IGuiProperties apply(CatalogScreen screen) {
+                return new mezz.jei.api.gui.handlers.IGuiProperties() {
+                    @Override public Class<? extends Screen> screenClass() { return CatalogScreen.class; }
+                    @Override public int guiXSize() { return (int)(260 * screen.calculateEffectiveScale()); }
+                    @Override public int guiYSize() { return (int)(260 * screen.calculateEffectiveScale()); }
+                    @Override public int guiLeft() { return (screen.width - guiXSize()) / 2; }
+                    @Override public int guiTop() { return (screen.height - guiYSize()) / 2; }
+                    @Override public int screenWidth() { return screen.width; }
+                    @Override public int screenHeight() { return screen.height; }
+                };
+            }
+        });
+
+        registration.addGlobalGuiHandler(new mezz.jei.api.gui.handlers.IGlobalGuiHandler() {
+            @Override
+            public Optional<? extends IClickableIngredient<?>> getClickableIngredientUnderMouse(IClickableIngredientFactory builder, double mouseX, double mouseY) {
+                if (Minecraft.getInstance().screen instanceof CatalogScreen catalog) {
+                    ItemStack hovered = catalog.getHoveredItem(mouseX, mouseY);
+
+                    if (hovered != null && !hovered.isEmpty()) {
+                        Rect2i area = new Rect2i((int) mouseX - 8, (int) mouseY - 8, 16, 16);
+                        return builder.createBuilder(hovered).buildWithArea(area);
+                    }
+                }
+                return Optional.empty();
+            }
+        });
+    }
+}
